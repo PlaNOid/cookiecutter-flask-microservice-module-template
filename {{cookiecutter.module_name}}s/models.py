98a2b6681aa1622b157bfeed3277c7d8568dbad9
Filipp <filin@beeline.ru>
2018-10-01 10:14:40 +0300

    flask module template

---
+++ {{cookiecutter.module_name}}s/models.py
@@ -0,0 +1,12 @@
+from flask_builder import db
+from datetime import datetime
+from sqlalchemy_serializer import SerializerMixin
+
+
+class {{cookiecutter.model_name}}(db.Model, SerializerMixin):
+    __tablename__ = '{{cookiecutter.module_name}}s'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
