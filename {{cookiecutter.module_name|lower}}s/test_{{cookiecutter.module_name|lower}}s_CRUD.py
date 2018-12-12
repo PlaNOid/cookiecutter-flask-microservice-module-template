30b72eacaf3b318aaf2d128aa73d4845a1440363
Yuri <426458+n0nSmoker@users.noreply.github.com>
2018-12-12 16:22:39 +0300

    Update test_{{cookiecutter.module_name|lower}}s_CRUD.py

--- {{cookiecutter.module_name|lower}}s/test_{{cookiecutter.module_name|lower}}s_CRUD.py
+++ {{cookiecutter.module_name|lower}}s/test_{{cookiecutter.module_name|lower}}s_CRUD.py
@@ -1,13 +1,15 @@
 from app.{{cookiecutter.module_name}}s.models import {{cookiecutter.module_name|capitalize}}
+from tests.utils import unpack
+
+
+module_name = {{cookiecutter.module_name|lower}}
 
 
 def test_create_{{cookiecutter.module_name|lower}}(client):
     resp = client.post(
-        endpoint='{{cookiecutter.module_name|lower}}s.add_{{cookiecutter.module_name|lower}}_view',
+        endpoint=f'{module_name}s.add_{module_name}_view',
     )
-
-    assert 'data' in resp
-    data = resp['data']
+    data = unpack(resp)
 
     assert 'id' in data
     assert {{cookiecutter.module_name|capitalize}}.query.filter_by(id=data['id']).one_or_none()
@@ -15,48 +17,36 @@ def test_create_{{cookiecutter.module_name|lower}}(client):
 
 def test_{{cookiecutter.module_name|lower}}_list(client):
     resp = client.get(
-        endpoint='{{cookiecutter.module_name|lower}}s.list_view',
+        endpoint=f'{module_name}s.list_view',
     )
-
-    assert 'data' in resp
-    data = resp['data']
-
+    data = unpack(resp)
     assert 'results' in data
     assert 'total' in data
 
 
 def test_{{cookiecutter.module_name|lower}}_by_id(client):
     resp = client.get(
-        endpoint='{{cookiecutter.module_name|lower}}s.{{cookiecutter.module_name|lower}}_by_id_view',
+        endpoint=f'{module_name}s.{module_name}_by_id_view',
         {{cookiecutter.module_name|lower}}_id={{cookiecutter.module_name|capitalize}}.query.first().id,
     )
-
-    assert 'data' in resp
-    data = resp['data']
-
+    data = unpack(resp)
     assert 'id' in data
 
 
 def test_update_{{cookiecutter.module_name|lower}}(client):
     resp = client.put(
-        endpoint='{{cookiecutter.module_name|lower}}s.update_{{cookiecutter.module_name|lower}}_view',
+        endpoint=f'{module_name}s.update_{module_name}_view',
         {{cookiecutter.module_name|lower}}_id={{cookiecutter.module_name|capitalize}}.query.first().id,
     )
-
-    assert 'data' in resp
-    data = resp['data']
-
+    data = unpack(resp)
     assert 'id' in data
     assert {{cookiecutter.module_name|capitalize}}.query.filter_by(id=data['id']).one().to_dict() == data
 
 
 def test_delete_{{cookiecutter.module_name|lower}}(client):
     resp = client.delete(
-        endpoint='{{cookiecutter.module_name|lower}}s.delete_{{cookiecutter.module_name|lower}}_view',
+        endpoint=f'{module_name}s.delete_{module_name}_view',
         {{cookiecutter.module_name|lower}}_id={{cookiecutter.module_name|capitalize}}.query.first().id,
     )
-
-    assert 'data' in resp
-    data = resp['data']
-
+    data = unpack(resp)
     assert not {{cookiecutter.module_name|capitalize}}.query.filter_by(id=data['id']).one_or_none()
